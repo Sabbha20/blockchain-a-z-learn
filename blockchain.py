b76c79a5ac442028dbfaf2cf36a7af7edc1f65d5
Sabbha20 <sabbha.power1@gmail.com>
2024-07-27 11:37:09 +0530

    implemented method: is_chain_valid.. Thus finishing the first portion of building blockchain

--- blockchain.py
+++ blockchain.py
@@ -39,3 +39,19 @@ class Blockchain:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_idx = 1
+        while block_idx < len(chain):
+            curr_block = chain[block_idx]
+            if curr_block["previous_hash"] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block["proof"]
+            curr_proof = curr_block["proof"]
+            hash_operation = hashlib.sha256(
+                str(curr_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] != "0000":
+                return False
+            previous_block = curr_block
+            block_idx += 1
