624064934ae23fd90a4595fe3a07eb78f40fe845
Sabbha20 <sabbha.power1@gmail.com>
2024-07-27 10:47:02 +0530

    implemented method: proof_of_work

--- blockchain.py
+++ blockchain.py
@@ -7,12 +7,12 @@ from flask import Flask, jsonify
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.create_block(proof = 1, prev_hash="0")
+        self.create_block(proof=1, prev_hash="0")
 
     def create_block(self, proof, prev_hash):
         block = {
-            "index":len(self.chain)+1,
-            "timestamp":str(datetime.datetime.now()),
+            "index": len(self.chain) + 1,
+            "timestamp": str(datetime.datetime.now()),
             "proof": proof,
             "previous_hash": prev_hash
         }
@@ -23,3 +23,15 @@ class Blockchain:
         if len(self.chain) == 0:
             return "Blockchain is empty"
         return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while not check_proof:
+            hash_operation = hashlib.sha256(
+                str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] == "0000":
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
